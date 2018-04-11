d30121ce3e07d663be55b2c9095a515259094223
James Reynolds <jameshreynolds@gmail.com>
2018-04-11 16:29:41 -0700

    game fully functional

--- js/main.js
+++ js/main.js
@@ -49,6 +49,16 @@ const isValidMove = (move, moves) => {
   return false;
 }
 
+checkForWin = (moves, player) => {
+  let winConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+  for (let i = 0; i < winConditions.length; i++) {
+    if (moves[winConditions[i][0]] === player && moves[winConditions[i][1]] === player && moves[winConditions[i][2]] === player) {
+      return true;
+    }
+  }
+  return false;
+}
+
 const namePosition = (num) => {
   let position = '';
   if (num <= '3') {
@@ -77,26 +87,44 @@ var schema = {
       }
     }
   };
- 
-  // 
-  // Start the prompt 
-  // 
-  prompt.start();
+  
  
   
   // Get two properties from the user: email, password 
-  prompt.get(schema, function (err, result) {
-    // 
-    // Log the results. 
-    // 
-    // console.log('Command-line input received:');
-    // console.log(typeof result.move)
-    console.log('  player selected: ' + namePosition(result.move));
-    positions[result.move.toString()] = 'X';
+  // prompt.get(schema, function (err, result) {
+  //   // 
+  //   // Log the results. 
+  //   // 
+  //   // console.log('Command-line input received:');
+  //   // console.log(typeof result.move)
+  //   console.log('  player selected: ' + namePosition(result.move));
+  //   positions[result.move.toString()] = 'X';
     
-    console.log(createBoard(positions));
+  //   console.log(createBoard(positions));
+  // });
+prompt.start();
+
+const playGame = (playerTurn, moves) => {
+  prompt.get(schema, function (err, result) {
+    if (isValidMove(result.move, moves)) {
+      console.log('  player ' + playerTurn + ' selected: ' + namePosition(result.move));
+      moves[result.move.toString()] = playerTurn;
+      console.log(createBoard(moves));
+      if (checkForWin(moves, playerTurn)) {
+        console.log(playerTurn + ' has won!')
+      } else {
+        if (playerTurn === 'X') {
+          playerTurn = 'O'
+        } else {
+          playerTurn = 'X'
+        }
+        playGame(playerTurn, moves)
+      }
+    } else {
+      console.log('That position has already been taken');
+      playGame(playerTurn, moves);
+    }
   });
-
-
-
+}
+playGame('X', positions)
   
